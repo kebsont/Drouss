9c2e3f14291b74db26c68f6bfa5bc0ff0d92b3bf
kebsont <mktapha@gmail.com>
2018-09-23 20:43:33 +0200

    Manager class to handle the creating of khassaides objects from API JSON

--- Dourouss/Model/KhassidasData.swift
+++ Dourouss/Model/KhassidasData.swift
@@ -8,3 +8,42 @@
 
 import Foundation
 import UIKit
+
+final class KhassidasData {
+
+static let sharedInstance = KhassidasData()
+fileprivate init() {}
+
+var khassidas: [Khassida] = []
+var images: [UIImage] = []
+
+func getKhassidas(completion: @escaping () -> Void) {
+    
+    APIClient.getKhassidasAPI { (json) in
+        let feed = json?["feed"] as? KhassidaJSON
+        if let results = feed?["results"] as? [KhassidaJSON] {
+            for dict in results {
+                let newKhassida = Khassida(dictionary: dict)
+                self.khassidas.append(newKhassida)
+            }
+            completion()
+        }
+    }
+}
+
+func getKhassidasImages(completion: @escaping () -> Void) {
+    getKhassidas {
+        for khassaide in self.khassidas {
+            let url = URL(string: khassaide.coverImage)
+            let data = try? Data(contentsOf: url!)
+            if let imageData = data {
+                let image = UIImage(data: imageData)
+                self.images.append(image!)
+            }
+        }
+        OperationQueue.main.addOperation {
+            completion()
+        }
+    }
+}
+}
